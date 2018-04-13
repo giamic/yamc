273a69da0960670dbdb7c8aba511f560490ba7e4
Gianluca Micchi <gianluca.micchi@gmail.com>
2018-04-13 17:59:48 +0200

    Fooling around.

--- main.py
+++ main.py
@@ -3,3 +3,15 @@ YAMC, yet another mnist classifier, is a project to learn the low-level tensorfl
 The data is taken from the website: http://yann.lecun.com/exdb/mnist/
 """
 
+import numpy as np
+import tensorflow as tf
+
+x = tf.placeholder(tf.float32)
+y = tf.placeholder(tf.float32)
+z = x + y
+
+writer = tf.summary.FileWriter('../board/')
+writer.add_graph(tf.get_default_graph())
+
+sess = tf.Session()
+print(sess.run(z, feed_dict={x:3, y:4}))
